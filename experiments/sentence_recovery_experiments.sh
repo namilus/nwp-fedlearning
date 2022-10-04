f45c0d24e56140e74548f2d12f79ae39586a1a50
Mohamed Suliman <sulimanm@tcd.ie>
2022-10-04 17:36:02 +0100

    all sentence recovery experiments

--- experiments/sentence_recovery_experiments.sh
+++ experiments/sentence_recovery_experiments.sh
@@ -84,36 +84,7 @@ python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 1000 -bs 32 -s 0.0 
 python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 1000 -bs 32 -s 0.0 -o sentence_recovery_results/
 
 
-# noisy dpsgd fedavg e = 50
-python3 sentence_recovery.py -f sample_datasets/16_0.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_1.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_2.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_3.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_4.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_5.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_6.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_7.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_8.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_9.txt -e 50 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_0.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_1.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_2.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_3.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_4.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_5.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_6.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_7.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/64_2.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/64_3.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/64_4.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/64_5.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/128_0.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/128_1.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 50 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-
-
-# noisy dpsgd fedavg e = 100
+# noisy dpsgd fedavg e = 100 s = 0.01
 python3 sentence_recovery.py -f sample_datasets/16_0.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
 python3 sentence_recovery.py -f sample_datasets/16_1.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
 python3 sentence_recovery.py -f sample_datasets/16_2.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
@@ -141,32 +112,87 @@ python3 sentence_recovery.py -f sample_datasets/128_1.txt -e 100 -bs 32 -s 0.01 
 python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
 python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
 
-# noisy dpsgd fedavg e = 1000
-python3 sentence_recovery.py -f sample_datasets/16_0.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_1.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_2.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_3.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_4.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_5.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_6.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_7.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_8.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/16_9.txt -e 1000 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_0.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_1.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_2.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_3.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_4.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_5.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_6.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/32_7.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/64_2.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/64_3.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/64_4.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/64_5.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/128_0.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/128_1.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
-python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 1000 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+# noisy dpsgd fedavg e = 100 s = 0.1
+python3 sentence_recovery.py -f sample_datasets/16_0.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_1.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_2.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_3.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_4.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_5.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_6.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_7.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_8.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_9.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_0.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_1.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_2.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_3.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_4.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_5.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_6.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_7.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_2.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_3.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_4.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_5.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/128_0.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/128_1.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
 
 
+# noisy single noisefedavg e = 100 s = 0.01
+python3 sentence_recovery.py -f sample_datasets/16_0.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_1.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_2.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_3.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_4.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_5.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_6.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_7.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_8.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_9.txt -e 100 -bs 16 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_0.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_1.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_2.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_3.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_4.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_5.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_6.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_7.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_2.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_3.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_4.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_5.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/128_0.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/128_1.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 100 -bs 32 -s 0.01 --cutoff -o sentence_recovery_results/ -dp 1
+
+# noisy single noisefedavg e = 100 s = 0.1
+python3 sentence_recovery.py -f sample_datasets/16_0.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_1.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_2.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_3.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_4.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_5.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_6.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_7.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_8.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/16_9.txt -e 100 -bs 16 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_0.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_1.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_2.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_3.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_4.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_5.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_6.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/32_7.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_2.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_3.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_4.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/64_5.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/128_0.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/128_1.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
+python3 sentence_recovery.py -f sample_datasets/256_0.txt -e 100 -bs 32 -s 0.1 --cutoff -o sentence_recovery_results/ -dp 1
